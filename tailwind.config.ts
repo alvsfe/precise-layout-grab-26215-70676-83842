1fd7b09fe6d2ad0a18a487445ec1cf95fdd3d11b
alvsfe <alvsgb440@gmail.com>
2025-10-19 12:10:59 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
-    container: <div className="overflow-x-hidden">
+    container: {"overflow-x-hidden">
       center: true,
       padding: "2rem",
       screens: {
